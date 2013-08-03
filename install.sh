cceb76d2e6333a94dc446111377ccdb697e86a59
Shantanu Goel <shantanu@shantanugoel.com>
2013-08-04 00:01:14 +0530

    Add tmux config

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ ln -sf "$(pwd)/bash/bashrc" ~/.bashrc
 ln -sf "$(pwd)/vim/vimrc" ~/.vimrc
 ln -sf "$(pwd)/vim/gvimrc" ~/.gvimrc
 
+ln -sf "$(pwd)/tmux.conf" ~/.tmux.conf
+
 test -d ~/.vim/ || ln -sfT "$(pwd)/vim/" ~/.vim
 
 git config --global core.excludesfile '~/.cvsignore'
