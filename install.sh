89d6217d39d420bda30450d8bbac974fecc601c6
Shantanu Goel <shantanu.goel@gmail.com>
2014-04-27 04:55:59 +0530

    move vimrc to _vimrc for compatibility with windows

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 ln -sf "$(pwd)/bash/bashrc" ~/.bashrc
 
-ln -sf "$(pwd)/vim/vimrc" ~/.vimrc
-ln -sf "$(pwd)/vim/gvimrc" ~/.gvimrc
+ln -sf "$(pwd)/vim/_vimrc" ~/.vimrc
+ln -sf "$(pwd)/vim/_gvimrc" ~/.gvimrc
 
 ln -sf "$(pwd)/tmux.conf" ~/.tmux.conf
 
